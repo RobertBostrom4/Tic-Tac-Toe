2a7230b0adcb52e74cb9024ee67c2fe1ac970457
RobertBostrom4 <Robertbostrom4@gmail.com>
2023-08-01 11:13:41 -0600

    Add object to represent the display controller

--- script.js
+++ script.js
@@ -84,3 +84,109 @@ const computer = (function (mark) {
     return { getMark, getPlayingStatus, move, playing, noLongerPlaying };
 });
 
+const displayController = (function () {
+
+    let gameBoardSection = document.querySelector(".gameboard-section");
+    let headerSection = document.querySelector(".header-section");
+    let person;
+    let artificialPerson;
+    let reset = document.querySelector(".reset-button");
+
+    const renderGameBoard = () => {
+
+        reset.addEventListener("click", (event) => {
+
+            person.playing();
+            artificialPerson.playing();
+
+            for (i = 0; i < gameBoard.getGameBoard().length; i++) {
+                for (j = 0; j < gameBoard.getGameBoard()[i].length; j++) {
+                    gameBoard.getGameBoard()[i][j] = "";
+                }
+            }
+
+            for (let section of gameBoardSection.children) {
+                section.textContent = "";
+            }
+
+
+        });
+
+        for (let button of headerSection.children) {
+            button.addEventListener("click", (event) => {
+                person = new player(button.textContent);
+
+                if (person.getMark() == "X") {
+                    artificialPerson = new computer("O");
+                } else {
+                    artificialPerson = new computer("X");
+                }
+
+                reset.click();
+            });
+        }
+
+        for (i = 0; i < gameBoard.getGameBoard().length; i++) {
+            for (j = 0; j < gameBoard.getGameBoard()[i].length; j++) {
+
+                let spotOnGameBoard = document.createElement("button");
+                spotOnGameBoard.className = "row- " + i + " -col- " + j;
+
+                let classChars = spotOnGameBoard.className.split(" ");
+                let row = parseInt(classChars[1]);
+                let column = parseInt(classChars[3]);
+
+                if (column == 0) {
+                    spotOnGameBoard.style.borderLeftStyle = "none";
+                } else if (column == 2) {
+                    spotOnGameBoard.style.borderRightStyle = "none";
+                }
+
+                spotOnGameBoard.addEventListener("click", (event) => {
+
+                    person.move(spotOnGameBoard);
+                    gameBoard.getGameBoard()[row][column] = person.getMark();
+
+
+                    //Add ai moves here using pseudorandom numbers
+                    artificialPerson.move(gameBoardSection);
+
+                    const diagOne = [gameBoard.getGameBoard()[0][2], gameBoard.getGameBoard()[1][1], gameBoard.getGameBoard()[2][0]];
+                    const diagTwo = [gameBoard.getGameBoard()[0][0], gameBoard.getGameBoard()[1][1], gameBoard.getGameBoard()[2][2]];
+                    const colOne = [gameBoard.getGameBoard()[0][0], gameBoard.getGameBoard()[1][0], gameBoard.getGameBoard()[2][0]];
+                    const colTwo = [gameBoard.getGameBoard()[0][1], gameBoard.getGameBoard()[1][1], gameBoard.getGameBoard()[2][1]];
+                    const colThree = [gameBoard.getGameBoard()[0][2], gameBoard.getGameBoard()[1][2], gameBoard.getGameBoard()[2][2]];
+
+
+                    const isMatchingPlayerMark = (currentvalue) => currentvalue == person.getMark();
+                    const isMatchingComputerMark = (currentvalue) => currentvalue == artificialPerson.getMark();
+
+                    if (gameBoard.getGameBoard()[row].every(isMatchingPlayerMark) || diagOne.every(isMatchingPlayerMark) || diagTwo.every(isMatchingPlayerMark) || colOne.every(isMatchingPlayerMark) || colTwo.every(isMatchingPlayerMark) || colThree.every(isMatchingPlayerMark)) {
+                        alert("You win!");
+                        person.noLongerPlaying();
+                        artificialPerson.noLongerPlaying();
+                    } else if (gameBoard.getGameBoard()[row].every(isMatchingComputerMark) || diagOne.every(isMatchingComputerMark) || diagTwo.every(isMatchingComputerMark) || colOne.every(isMatchingComputerMark) || colTwo.every(isMatchingComputerMark) || colThree.every(isMatchingComputerMark)) {
+                        alert("You lose!");
+                        person.noLongerPlaying();
+                        artificialPerson.noLongerPlaying();
+                    }
+
+                });
+
+                gameBoardSection.appendChild(spotOnGameBoard);
+            }
+
+        }
+    }
+
+    gameBoardSection
+    return {
+        renderGameBoard
+    };
+
+})();
+
+displayController.renderGameBoard();
+
+
+
