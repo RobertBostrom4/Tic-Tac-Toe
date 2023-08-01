5e799410b73040bfdd8631beabe88fcc8148d542
RobertBostrom4 <Robertbostrom4@gmail.com>
2023-08-01 11:10:22 -0600

    Add object to represent each player

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ const gameBoard = (function () {
     };
 
 })();
+
+const player = (function (mark) {
+
+    let isPlaying = true;
+
+    const noLongerPlaying = () => isPlaying = false;
+
+    const playing = () => isPlaying = true;
+
+    const getPlayingStatus = () => isPlaying;
+
+    const getMark = () => mark;
+
+    const move = (spotOnGameBoard) => {
+
+        if (isPlaying) {
+
+            if (spotOnGameBoard.textContent == "") {
+                spotOnGameBoard.textContent = mark;
+            }
+        }
+
+    }
+
+    return {
+        getMark,
+        move,
+        noLongerPlaying,
+        playing,
+        getPlayingStatus
+    }
+
+});
